7ae57049c5474ed546076537b17a2733bcae53ff
Ryan Watkins-Hughes <ryanwatkinshughes@gmail.com>
2019-12-04 10:07:13 -0600

    Sort cocktail list before dislaying

--- client/src/components/CocktailList/index.js
+++ client/src/components/CocktailList/index.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { gql } from "apollo-boost";
-import { useQuery } from '@apollo/react-hooks';
+import { useQuery } from "@apollo/react-hooks";
 import Cocktail from "../Cocktail";
 import styles from "./styles.module.css";
 
@@ -15,21 +15,43 @@ const GET_COCKTAILS = gql`
 
 export default function CocktailList() {
   const { loading, error, data } = useQuery(GET_COCKTAILS);
-  
-  const filterList = () => {
-    return true;
-  }
+  const [dataSorted, setDataSorted] = useState(false);
+
+  const sortData = data => {
+    return data.sort(function(a, b) {
+      if (a.name < b.name) {
+        return -1;
+      }
+      if (a.name > b.name) {
+        return 1;
+      }
+      return 0;
+    });
+  };
+
+  useEffect(() => {
+    if (data) {
+      sortData(data.cocktails);
+      setDataSorted(true);
+    }
+  }, [data]);
+
+  const filterData = event => {
+    const typedWord = event.target.value;
+    debugger;
+  };
 
   if (loading) {
     return (
       <div className="loading">
         <span className="visuallyHidden">Loading</span>
-    </div>)
+      </div>
+    );
   }
   if (error) {
-    return <p>Error</p>
+    return <p>Error</p>;
   }
-  
+
   return (
     <section className={styles.cocktailList}>
       <form>
@@ -42,16 +64,20 @@ export default function CocktailList() {
               type="text"
               className="Search"
               placeholder="Search by ingredient"
-              onChange={filterList}
+              onChange={filterData}
             />
           </label>
         </fieldset>
       </form>
-      { data.cocktails.map(cocktail => {
-        return (
-         <Cocktail key={cocktail.id} id={cocktail.id} name={cocktail.name} />
-        );
-        })}
-        </section>
-    )
+      {dataSorted ? (
+        data.cocktails.map(cocktail => {
+          return (
+            <Cocktail key={cocktail.id} id={cocktail.id} name={cocktail.name} />
+          );
+        })
+      ) : (
+        <div />
+      )}
+    </section>
+  );
 }
