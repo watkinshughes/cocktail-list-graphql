2d6267c627b53e4ff4388e874f4d609b3936db0b
Ryan Watkins-Hughes <ryanwatkinshughes@gmail.com>
2019-12-12 16:46:52 -0600

    Web: Add view recipe text

--- web-client/src/components/Cocktail/index.js
+++ web-client/src/components/Cocktail/index.js
@@ -6,7 +6,9 @@ export default function Cocktail(props) {
   return (
     <article className={styles.cocktail}>
       <Link to={props.id}>
-        <h1>{props.name}</h1>
+        <h1>
+          <span>{props.name}</span> <small>view recipe →</small>
+        </h1>
       </Link>
     </article>
   );
